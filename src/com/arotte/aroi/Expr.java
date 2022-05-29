4bad51e45b69a7f132070aad62a6da548f181f9f
Aron Molnar <thearote@gmail.com>
2022-05-29 15:48:36 +0100

    regenerate AST with new logical operators class

--- src/com/arotte/aroi/Expr.java
+++ src/com/arotte/aroi/Expr.java
@@ -13,6 +13,7 @@ abstract class Expr {
   interface Visitor<R> {
     R visitAssignExpr(Assign expr);
     R visitLiteralExpr(Literal expr);
+    R visitLogicalExpr(Logical expr);
     R visitGroupingExpr(Grouping expr);
     R visitUnaryExpr(Unary expr);
     R visitBinaryExpr(Binary expr);
@@ -46,6 +47,24 @@ abstract class Expr {
     @Override
     <R> R accept(Visitor<R> visitor) {
       return visitor.visitLiteralExpr(this);
+    }
+  }
+
+  static class Logical extends Expr {
+    // fields
+    final Expr left;
+    final Token operator;
+    final Expr right;
+    // constructor
+    Logical(Expr left, Token operator, Expr right) {
+      this.left = left;
+      this.operator = operator;
+      this.right = right;
+    }
+    // visitor pattern
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitLogicalExpr(this);
     }
   }
 
