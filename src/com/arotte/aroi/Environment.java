909621b7135d33a44d59d7051cb176a892bb834f
Aron Molnar <thearote@gmail.com>
2022-05-29 14:57:36 +0100

    feat: start adding support for scopes

--- src/com/arotte/aroi/Environment.java
+++ src/com/arotte/aroi/Environment.java
@@ -4,12 +4,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Environment {
+    final Environment enclosing;
     private final Map<String, Object> values = new HashMap<>();
+
+    Environment() {
+        this.enclosing = null;
+    }
+
+    Environment(Environment environment) {
+        this.enclosing = environment;
+    }
 
     Object get(Token name) {
         // return value of variable
         if (values.containsKey(name.lexeme))
             return values.get(name.lexeme);
+
+        // Recursive step:
+        // if variable is not present in current local scope,
+        // get it from the enclosing scope
+        if (enclosing != null)
+            return enclosing.get(name);
 
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
     }
@@ -22,6 +37,13 @@ class Environment {
     void assign(Token name, Object value) {
         if (values.containsKey(name.lexeme)) {
             values.put(name.lexeme, value);
+            return;
+        }
+
+        // Recursive step:
+        // if variable is not present in current scope
+        if (enclosing != null) {
+            enclosing.assign(name, value);
             return;
         }
 
